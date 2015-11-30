27fc9e49ff4780a24c278f1e909b879ba30ae105
ningsiping <ningsp@yimayholiday.com>
2015-11-30 10:08:05 +0800

    modify daxiao

--- membercenter-api/src/main/java/com/yimayhd/membercenter/MemberReturnCode.java
+++ membercenter-api/src/main/java/com/yimayhd/membercenter/MemberReturnCode.java
@@ -73,19 +73,19 @@ public class MemberReturnCode  extends AbstractReturnCode {
 
 
 	/**************************************merchant********************************************/
-	public static final int MERCHANT_NOT_FOUND_ERROR_C = 160004000;
+	public static final int MERCHANT_NOT_FOUND_ERROR_C = 16004000;
 	public static final MemberReturnCode MERCHANT_NOT_FOUND_ERROR = new MemberReturnCode(MERCHANT_NOT_FOUND_ERROR_C, "商家不存在") ;
 	
 	
 	
 	/**************************************member********************************************/
-	public static final int MEMBER_NOT_FOUND_C = 160005000;
+	public static final int MEMBER_NOT_FOUND_C = 16005000;
 	public static final MemberReturnCode MEMBER_NOT_FOUND = new MemberReturnCode(MEMBER_NOT_FOUND_C, "会员信息未找到") ;
 	
 	
 	
 	/**************************************item********************************************/
-	public static final int PAGE_QUERY_ITEM_FAILED_C = 160006000;
+	public static final int PAGE_QUERY_ITEM_FAILED_C = 16006000;
 	public static final MemberReturnCode PAGE_QUERY_ITEM_FAILED = new MemberReturnCode(PAGE_QUERY_ITEM_FAILED_C, "分页查询商品信息失败") ;
 	
 	

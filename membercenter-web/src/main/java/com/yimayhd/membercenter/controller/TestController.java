9516534208d60b2cb35520f7255d855a54084451
ningsiping <ningsp@yimayholiday.com>
2015-11-30 15:00:10 +0800

    test sendphone

--- membercenter-web/src/main/java/com/yimayhd/membercenter/controller/TestController.java
+++ membercenter-web/src/main/java/com/yimayhd/membercenter/controller/TestController.java
@@ -1,8 +1,12 @@
 package com.yimayhd.membercenter.controller;
 
+import com.sun.org.apache.xpath.internal.operations.Bool;
+import com.yimayhd.user.client.result.BaseResult;
+import com.yimayhd.user.client.service.UserService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -12,9 +16,15 @@ import java.util.Map;
 @RestController
 public class TestController {
 
+    @Resource
+    private UserService userService;
+
     @RequestMapping("/test/send")
     public Map<String, Object> test() {
         Map<String, Object> map = new HashMap<>();
+        BaseResult<Boolean> baseResult = userService.sendPhoneVerifyCode("18601907819");
+
+        map.put("result", baseResult.getValue());
         return map;
     }
 }

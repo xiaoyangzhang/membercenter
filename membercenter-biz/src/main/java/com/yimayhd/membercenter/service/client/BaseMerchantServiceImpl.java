3367d88778b442e70ae5fe77d846f77e41486128
wuzhengfei <wuzf@yimayholiday.com>
2015-12-07 10:37:48 +0800

    bugfix

--- membercenter-biz/src/main/java/com/yimayhd/membercenter/service/client/BaseMerchantServiceImpl.java
+++ membercenter-biz/src/main/java/com/yimayhd/membercenter/service/client/BaseMerchantServiceImpl.java
@@ -1,5 +1,7 @@
 package com.yimayhd.membercenter.service.client;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.yiholiday.fhtd.logger.annot.MethodLogger;
@@ -13,19 +15,25 @@ import com.yimayhd.membercenter.manager.BaseMerchantManager;
  * Created by Administrator on 2015/12/1.
  */
 public class BaseMerchantServiceImpl implements BaseMerchantService {
-
+	private static final Logger logger = LoggerFactory.getLogger("BaseMerchantServiceImpl");
 
     @Autowired
     private BaseMerchantManager baseMerchantManager;
 
     @Override
-    @MethodLogger(isCatchException = false, isPrintArguments = true, isPrintResult = true)
+    @MethodLogger(isPrintArguments = true, isPrintResult = true)
     public MemResult<BaseMerchantDO> getMerchantByUserId(long userId) {
-        MemResult<BaseMerchantDO> rs = new MemResult<BaseMerchantDO>();
-        if(userId<=0){
-            rs.setReturnCode(MemberReturnCode.PARAMTER_ERROR);
-            return rs;
-        }
-        return baseMerchantManager.getBaseMerchantByUserId(userId);
+    	MemResult<BaseMerchantDO> rs = new MemResult<BaseMerchantDO>();
+    	try{
+    		if(userId<=0){
+    			rs.setReturnCode(MemberReturnCode.PARAMTER_ERROR);
+    			return rs;
+    		}
+    		rs =  baseMerchantManager.getBaseMerchantByUserId(userId);
+    	}catch(Exception e){
+    		logger.error("getBaseMerchantByUserId failed!  userId={}", userId, e);
+    		rs.setReturnCode(MemberReturnCode.SYSTEM_ERROR);
+    	}
+    	return rs ;
     }
 }
--- membercenter-biz/src/main/java/com/yimayhd/membercenter/service/client/MemberServiceImpl.java
+++ membercenter-biz/src/main/java/com/yimayhd/membercenter/service/client/MemberServiceImpl.java
@@ -1,28 +1,45 @@
 package com.yimayhd.membercenter.service.client;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.yiholiday.fhtd.logger.annot.MethodLogger;
+import com.yimayhd.membercenter.MemberReturnCode;
 import com.yimayhd.membercenter.client.domain.MemberDO;
 import com.yimayhd.membercenter.client.result.MemResult;
 import com.yimayhd.membercenter.client.service.MemberService;
 import com.yimayhd.membercenter.manager.MemberManager;
 
 public class MemberServiceImpl implements MemberService {
-
+	private static final Logger logger = LoggerFactory.getLogger("MemberServiceImpl");
 	@Autowired
 	private MemberManager memberManager ;
 	
 	@Override
-	@MethodLogger(isCatchException = false, isPrintArguments = true, isPrintResult = true)
+	@MethodLogger(isPrintArguments = true, isPrintResult = true)
 	public MemResult<MemberDO> getMemberById(long id) {
-		return memberManager.getMemberById(id);
+		MemResult<MemberDO> result = new MemResult<MemberDO>() ;
+		try {
+			result = memberManager.getMemberById(id);
+		} catch (Exception e) {
+			logger.error("getMemberById failed!  id={}",id, e);
+			result.setReturnCode(MemberReturnCode.SYSTEM_ERROR);
+		}
+		return result ;
 	}
 
 	@Override
-	@MethodLogger(isCatchException = false, isPrintArguments = true, isPrintResult = true)
+	@MethodLogger(isPrintArguments = true, isPrintResult = true)
 	public MemResult<Boolean> overdueMember(long id) {
-		return memberManager.overdueMember(id);
+		MemResult<Boolean> result = new MemResult<Boolean>() ;
+		try {
+			result = memberManager.overdueMember(id);
+		} catch (Exception e) {
+			logger.error("overdueMember failed!  id={}",id, e);
+			result.setReturnCode(MemberReturnCode.SYSTEM_ERROR);
+		}
+		return result ;
 	}
 
 }

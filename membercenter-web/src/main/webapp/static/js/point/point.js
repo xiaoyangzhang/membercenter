e1f67822c9a7f800526f6fa994cbab4a22970735
taoxiaoxian <tangqx@yimayholiday.com>
2015-12-08 16:13:57 +0800

    积分页面调整

--- membercenter-web/src/main/webapp/static/js/point/point.js
+++ membercenter-web/src/main/webapp/static/js/point/point.js
@@ -1,5 +1,5 @@
-
 var contextPath=$("#contextPath").val();
+var myScroll, pullUpEl, pullUpOffset, generatedCount = 0;
 
 /*#####################################ajax 后台交互:start##########################################*/
 
@@ -11,7 +11,7 @@ function getCurrentPoint( callback) {
 }
 
 function getPointDetailsByPage(pageNumber, pageSize,
-		callback) {
+							   callback) {
 	// 分页获取积分明细
 	$.post(contextPath + "/point/memberPointDetails", {
 		pageNumber : pageNumber,
@@ -24,20 +24,6 @@ function getPointDetailsByPage(pageNumber, pageSize,
 /*#####################################ajax 后台交互:end##########################################*/
 
 
-/*############################积分明细页面:start##############################################*/
-function pointDetailLoaded() {
-	pullUpEl = document.getElementById('pullUp');	
-	pullUpOffset = pullUpEl.offsetHeight;
-	
-	myScroll = new iScroll('wrapper', {
-		useTransition: true,
-		onScrollEnd: function () {
-			pointDetailPullUpAction();	
-		}
-	});
-	
-} 
-
 function pointDetailPullUpAction () {
 	//页号
 	var pageNumber = $("#pageNumber").val();
@@ -45,7 +31,7 @@ function pointDetailPullUpAction () {
 	var pageSize = $("#pageSize").val();
 	var userId = $("#userId").val();
 	var merchantId = $("#merchantId").val();
-	
+
 	//获取当前积分
 	getCurrentPoint(function(data){
 		var isSuccessful = data.meta.success;
@@ -57,8 +43,8 @@ function pointDetailPullUpAction () {
 	});
 
 	//获取积分明细
-	getPointDetailsByPage(pageNumber, pageSize, function(
-			data) {
+	/*getPointDetailsByPage(pageNumber, pageSize, function(
+		data) {
 		var isSuccessful = data.meta.success;
 		if (isSuccessful == true) {
 			var pointDetails = data.data.pointDetails;
@@ -67,42 +53,22 @@ function pointDetailPullUpAction () {
 
 			for (i in pointDetails) {
 				var detailStr = " <li> <span class='left'>"
-						+ pointDetails[i].source + "&nbsp; <br/><em>"
-						+ pointDetails[i].createDate
-						+ "</em></span><span class='right'> "
-						+ pointDetails[i].type + pointDetails[i].point
-						+ "<br/> <em>有效期至" + pointDetails[i].endDate
-						+ "</em></span></li> ";
-				$("#showPointDetailsDiv").append(detailStr);
+					+ pointDetails[i].source + "&nbsp; <br/><em>"
+					+ pointDetails[i].createDate
+					+ "</em></span><span class='right'> "
+					+ pointDetails[i].type + pointDetails[i].point
+					+ "<br/> <em>有效期至" + pointDetails[i].endDate
+					+ "</em></span></li> ";
+				$("#thelist").append(detailStr);
 			}
 		}else{
 			alert("获取积分明细错误:" + data.meta.message);
 		}
-		
+
 		myScroll.refresh();
-	});
-}
-
-
-function initPointDetails(){
-	//页号
-	var pageNumber = $("#pageNumber").val();
-	//每页显示多少条
-	var pageSize = $("#pageSize").val();
-	
-	//获取当前积分
-	getCurrentPoint(function(data){
-		var isSuccessful = data.meta.success;
-		if (isSuccessful == true) {
-			$("#currentPoint").text(data.data.totalPoint);
-		}else{
-			alert("获取总积分错误:" + data.meta.message);
-		}
-	});
-
-	//获取积分明细
+	});*/
 	getPointDetailsByPage(pageNumber, pageSize, function(
-			data) {
+		data) {
 		var isSuccessful = data.meta.success;
 		if (isSuccessful == true) {
 			var pointDetails = data.data.pointDetails;
@@ -111,23 +77,79 @@ function initPointDetails(){
 
 			for (i in pointDetails) {
 				var detailStr = " <li> <span class='left'>"
-						+ pointDetails[i].source + "&nbsp; <br/><em>"
-						+ pointDetails[i].createDate
-						+ "</em></span><span class='right'> "
-						+ pointDetails[i].type + pointDetails[i].point
-						+ "<br/> <em>有效期至" + pointDetails[i].endDate
-						+ "</em></span></li> ";
-				$("#showPointDetailsDiv").append(detailStr);
+					+ pointDetails[i].source + "&nbsp; <br/><em>"
+					+ pointDetails[i].createDate
+					+ "</em></span><span class='right'> "
+					+ pointDetails[i].type + pointDetails[i].point
+					+ "<br/> <em>有效期至" + pointDetails[i].endDate
+					+ "</em></span></li> ";
+				$("#theList").append(detailStr);
 			}
 		}else{
 			alert("获取积分明细错误:" + data.meta.message);
 		}
 	});
-
-	document.addEventListener('touchmove', function (e) { e.preventDefault(); }, false);
-
-	document.addEventListener('DOMContentLoaded', function () { setTimeout(pointDetailLoaded, 200); }, false);
 }
 
 
-/*############################积分明细页面:end##############################################*/
+
+
+//function pullUpAction () {
+//	setTimeout(function () {	// <-- Simulate network congestion, remove setTimeout from production!
+//		var el, li, i;
+//		el = document.getElementById('thelist');
+//
+//		for (i=0; i<3; i++) {
+//			li = document.createElement('li');
+//			li.innerText = " <li> <span class='left'>"
+//				+ pointDetails[i].source + "&nbsp; <br/><em>"
+//				+ pointDetails[i].createDate
+//				+ "</em></span><span class='right'> "
+//				+ pointDetails[i].type + pointDetails[i].point
+//				+ "<br/> <em>有效期至" + pointDetails[i].endDate
+//				+ "</em></span></li> ";
+//			el.appendChild(li, el.childNodes[0]);
+//		}
+//
+//		myScroll.refresh();		// Remember to refresh when contents are loaded (ie: on ajax completion)
+//	}, 1000);	// <-- Simulate network congestion, remove setTimeout from production!
+//}
+
+function loaded() {
+	pullUpEl = document.getElementById('pullUp');
+	pullUpOffset = pullUpEl.offsetHeight;
+
+	myScroll = new iScroll('wrapper', {
+		useTransition: true,
+		onRefresh: function () {
+			if (pullUpEl.className.match('loading')) {
+				pullUpEl.className = '';
+				pullUpEl.querySelector('.pullUpLabel').innerHTML = '加载更多...';
+			}
+		},
+		onScrollMove: function () {
+			if (this.y < (this.maxScrollY - 5) && !pullUpEl.className.match('flip')) {
+				pullUpEl.className = 'flip';
+				pullUpEl.querySelector('.pullUpLabel').innerHTML = '放开刷新...';
+				this.maxScrollY = this.maxScrollY;
+			} else if (this.y > (this.maxScrollY + 5) && pullUpEl.className.match('flip')) {
+				pullUpEl.className = '';
+				pullUpEl.querySelector('.pullUpLabel').innerHTML = '加载更多...';
+				this.maxScrollY = pullUpOffset;
+			}
+		},
+		onScrollEnd: function () {
+			if (pullUpEl.className.match('flip')) {
+				pullUpEl.className = 'loading';
+				pullUpEl.querySelector('.pullUpLabel').innerHTML = '加载中...';
+				pointDetailPullUpAction();	// Execute custom function (ajax call?)
+			}
+		}
+	});
+
+	setTimeout(function () { document.getElementById('wrapper').style.left = '0'; }, 800);
+}
+
+document.addEventListener('touchmove', function (e) { e.preventDefault(); }, false);
+
+document.addEventListener('DOMContentLoaded', function () { setTimeout(loaded, 200); }, false);

803d0039cfa892a9bfd8b4f1e8436a084fc8140e
hanlei <hanlei@jiuxiulvxing.com>
2016-06-01 16:27:11 +0800

    删除作废代码

--- membercenter-biz/src/main/java/com/yimayhd/membercenter/service/client/BusinessScopeServiceImpl.java
+++ membercenter-biz/src/main/java/com/yimayhd/membercenter/service/client/BusinessScopeServiceImpl.java
@@ -1,17 +1,16 @@
 package com.yimayhd.membercenter.service.client;
 
-import java.util.List;
-
-import org.apache.ibatis.annotations.Param;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-
 import com.alibaba.fastjson.JSON;
+import com.yimayhd.membercenter.MemberReturnCode;
 import com.yimayhd.membercenter.client.domain.merchant.BusinessScopeDO;
 import com.yimayhd.membercenter.client.result.MemResult;
 import com.yimayhd.membercenter.client.service.BusinessScopeService;
 import com.yimayhd.membercenter.manager.BusinessScopeManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
 
 /**
  * Created by hanlei on 2016/5/24.
@@ -20,19 +19,20 @@ public class BusinessScopeServiceImpl implements BusinessScopeService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ScopeItemCategoryServiceImpl.class);
 
-	@Autowired
-	private BusinessScopeManager businessScopeManager;
+    @Autowired
+    private BusinessScopeManager businessScopeManager;
 
-	@Override
-	public MemResult<List<BusinessScopeDO>> findBusinessScopesByScope(BusinessScopeDO businessScope,List<Long> idList) {
+    @Override
+    public MemResult<List<BusinessScopeDO>> findBusinessScopesByScope(BusinessScopeDO businessScope, List<Long> idList) {
+        MemResult<List<BusinessScopeDO>> result = new MemResult<>();
         if (businessScope == null) {
             LOGGER.info("businessScopeDOs not found by businessScope={}", JSON.toJSONString(businessScope));
-            return MemResult.buildFailResult(0, "参数为空", null);
+            result.setReturnCode(MemberReturnCode.PARAMTER_ERROR);
+            return result;
         }
-		List<BusinessScopeDO> businessScopeDOs = businessScopeManager.getBusinessScopesByScope(businessScope,idList);
-		MemResult<List<BusinessScopeDO>> result = new MemResult<>();
-		result.setValue(businessScopeDOs);
-		return result;
-	}
+        List<BusinessScopeDO> businessScopeDOs = businessScopeManager.getBusinessScopesByScope(businessScope, idList);
+        result.setValue(businessScopeDOs);
+        return result;
+    }
 
 }

437a6ba36774365a1ddd7ffe2184820f2b55125c
hanlei <hanlei@jiuxiulvxing.com>
2016-06-04 21:04:13 +0800

    商家入驻审批，重新定义接口，面向运营后台和商家后台

--- membercenter-client/src/main/java/com/yimayhd/membercenter/client/service/MerchantItemCategoryService.java
+++ membercenter-client/src/main/java/com/yimayhd/membercenter/client/service/MerchantItemCategoryService.java
@@ -1,14 +1,14 @@
 package com.yimayhd.membercenter.client.service;
 
-import java.util.List;
-
 import com.yimayhd.membercenter.client.domain.merchant.MerchantItemCategoryDO;
 import com.yimayhd.membercenter.client.result.MemResult;
 import com.yimayhd.membercenter.client.result.MemResultSupport;
 
+import java.util.List;
+
 public interface MerchantItemCategoryService {
 
-	MemResult<List<MerchantItemCategoryDO>> findMerchantItemCategoriesByExamineId(int domainId, long examineId);
+	MemResult<List<MerchantItemCategoryDO>> findMerchantItemCategoriesBySellerId(int domainId, long sellerId);
 	
 	MemResultSupport saveMerchantItemCategories(int domainId, long examineId, long[] categoryIds);
 }

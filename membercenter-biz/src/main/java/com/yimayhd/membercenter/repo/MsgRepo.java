9144dabe53459ec8377646d533449cc32a4c37db
zhangxiaoyang <xiaoyangzhang1990@163.com>
2016-06-21 21:01:15 +0800

    1

--- membercenter-biz/src/main/java/com/yimayhd/membercenter/repo/MsgRepo.java
+++ membercenter-biz/src/main/java/com/yimayhd/membercenter/repo/MsgRepo.java
@@ -1,10 +1,13 @@
 package com.yimayhd.membercenter.repo;
 
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.yimayhd.msgcenter.client.domain.PushRecordDO;
+import com.yimayhd.msgcenter.client.enums.ErrorCode;
 import com.yimayhd.msgcenter.client.param.SendSmsOption;
 import com.yimayhd.msgcenter.client.result.BaseResult;
 import com.yimayhd.msgcenter.client.service.MsgCenterService;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,14 +31,15 @@ public class MsgRepo {
     public boolean sendPush(PushRecordDO pushRecordDO) {
         try {
             BaseResult<Boolean> baseResult = msgCenterService.sendPush(pushRecordDO);
-            // if (baseResult != null && baseResult.isSuccess()) {
-            // result = baseResult.getValue();
-            // } else if (baseResult != null) {
-            // logger.error("MsgRepo pushMsg error:{}",baseResult.getResultMsg());
-            // }
-            logger.info("par:{} sendPush return :{}", JSONObject.toJSONString(pushRecordDO),
-                    JSONObject.toJSONString(baseResult));
-            return baseResult.isSuccess();
+            logger.info("par:{} sendPush return :{}", JSONObject.toJSONString(pushRecordDO), JSONObject.toJSONString(baseResult));
+            if (baseResult == null || !baseResult.isSuccess()) {
+            	 logger.error("MsgRepo pushMsg pushRecordDO={}  result={}",JSON.toJSONString(pushRecordDO), JSON.toJSONString(baseResult));
+            	 if( baseResult != null && ErrorCode.USER_PUSH_NOT_REGISTRATION.name().equals( baseResult.getErrorCode() ) ) {
+            		 return true;
+            	 }
+            	 return false;
+             }
+            return true;
         } catch (Exception e) {
             logger.error("par:{} sendPush failed, error:{}", JSONObject.toJSONString(pushRecordDO), e);
         }

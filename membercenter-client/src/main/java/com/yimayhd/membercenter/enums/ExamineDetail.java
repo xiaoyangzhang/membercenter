076b0b1982e68c7109d784185d9ef89c5b387ead
zhangxiaoyang <xiaoyangzhang1990@163.com>
2016-07-29 14:10:38 +0800

    1

--- membercenter-client/src/main/java/com/yimayhd/membercenter/enums/ExamineDetail.java
+++ membercenter-client/src/main/java/com/yimayhd/membercenter/enums/ExamineDetail.java
@@ -53,8 +53,10 @@ public enum ExamineDetail {
     
     
     //**********店铺名称************************//
-    MERCHANT_NAME("店铺名称", "21", 1);
-    
+    MERCHANT_NAME("店铺名称", "21", 1),
+    ACCOUNT_TYPE("账户类型","25",1),
+    OPENER_CARD("开户人身份证","26",1),
+    SETTLEMENT_CARD("结算联行号","27",1);
     private String name;
     private String id;
     private int number;

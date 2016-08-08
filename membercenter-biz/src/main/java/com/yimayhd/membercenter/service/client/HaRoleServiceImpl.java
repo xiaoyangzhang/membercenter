56f66a5cc44711f66c08ae3e1d03e0aa8fb7103b
xushubing <xusb@jiuxiulvxing.com>
2016-08-08 18:16:38 +0800

    菜单角色

---
+++ membercenter-biz/src/main/java/com/yimayhd/membercenter/service/client/HaRoleServiceImpl.java
@@ -0,0 +1,9 @@
+package com.yimayhd.membercenter.service.client;
+
+import com.yimayhd.membercenter.client.service.HaRoleService;
+
+/**
+ * Created by xushubing on 2016/8/8.
+ */
+public class HaRoleServiceImpl implements HaRoleService {
+}
